344691c4cc6d9e518f104c81f2d79d86aa5bdd68
ZoloMohan <mohanzolomon@gmail.com>
2019-07-25 20:12:04 +0530

    Updates

---
+++ precache-manifest.df7d18e5a17ba71bd524c994c5661ce6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc381fd76e2254e89ee4cff132ed41ea",
+    "url": "/lights-out/index.html"
+  },
+  {
+    "revision": "4cf19168709adadfd2e8",
+    "url": "/lights-out/static/css/main.5990adbd.chunk.css"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/lights-out/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "4cf19168709adadfd2e8",
+    "url": "/lights-out/static/js/main.a4132d9c.chunk.js"
+  },
+  {
+    "revision": "d6b7daafe64bb82a9237",
+    "url": "/lights-out/static/js/runtime~main.69e4817a.js"
+  }
+]);
